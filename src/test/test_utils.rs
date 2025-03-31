8cb0f1ece51c4e6884243db782b43a5cc3e94984
KanjiNaem <janik.jehmlich@gmail.com>
2025-03-31 21:59:56 +0200

    update test_utils :)

--- src/test/test_utils.rs
+++ src/test/test_utils.rs
@@ -1,1 +1,40 @@
-pub const TEST_MP4_PATH: &str = "/home/kanjinaem/coding/emoterm/src/test/qtEmoBoyDancingTest.mp4"; 
+use opencv::{
+    prelude::*, 
+    videoio::{
+        self, 
+        VideoCapture, 
+        highgui,
+    }, 
+    Result,
+};
+
+#[allow(dead_code)]
+pub fn vid_cap_from_mp4_in_player_test(file_path: &str) -> Result<()> {
+    let win_name = "capture_window";
+    highgui::named_window(win_name, highgui::WINDOW_AUTOSIZE)?;
+    let mut vid_player = VideoCapture::from_file_def(file_path)?;
+    let opened = videoio::VideoCapture::is_opened(&vid_player)?;
+    if !opened {
+        panic!("////////////////////// can't open vid file :( //////////////////////");
+    } 
+
+    let vid_fps: f64 = vid_player.get(CAP_PROP_FPS)?;
+    let frame_delay: i32 = (1000.0 / vid_fps) as i32; 
+
+    loop {
+        let mut curr_frame = Mat::default();
+        vid_player.read(&mut curr_frame)?;
+        if curr_frame.size()?.width > 0 {
+            highgui::imshow(win_name, &curr_frame)?;
+        }
+        
+        let key = highgui::wait_key(frame_delay)?;
+        if key == 113 {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+// pub const TEST_MP4_PATH: &str = "/home/kanjinaem/coding/emoterm/src/test/qtEmoBoyDancingTest.mp4"; 
