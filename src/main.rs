f757f06fb71f4910a1b7279b91273d43aeeb1338
KanjiNaem <janik.jehmlich@gmail.com>
2025-04-01 01:41:48 +0200

    add structure + update dependencies :)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-// #[path = "service/frame_extraction.rs"] mod frames;
+// #[path = "service_frame/frame_extraction.rs"] mod frames;
 // #[path = "test/test_utils.rs"] mod test;
 // #[path = "utils"] mod utils;
-mod service;
+mod service_terminal;
+mod service_frame;
 mod test;
 mod utils; // ok so this def looks stupid and ill prob have to redo this later
 
 
 fn main() {
     // frame::vid_cap_from_mp4("/home/kanjinaem/coding/emoterm/src/test/qtEmoGuyDancingTest.mp4").unwrap();
-    service::frame_extraction::vid_cap_from_mp4_test("/home/kanjinaem/coding/emoterm/src/test/qtEmoGuyDancingTest.mp4").unwrap();
+    service_frame::frame_extraction::vid_cap_from_mp4_test("/home/kanjinaem/coding/emoterm/src/test/qtEmoGuyDancingTest.mp4").unwrap();
 }
 
 // somehow works if uri is passed directly as string ref, but not if passed from const string ref test const
