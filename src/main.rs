db7dda82da8dd492d4f0bc659dc7aed8f1bdd630
KanjiNaem <janik.jehmlich@gmail.com>
2025-03-31 14:07:00 +0200

    mess around with Mat types :)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 
 
 fn main() {
-    frame::get_vid_from_mp4("/home/kanjinaem/coding/emoterm/src/test/qtEmoGuyDancingTest.mp4").unwrap();
+    // frame::vid_cap_from_mp4("/home/kanjinaem/coding/emoterm/src/test/qtEmoGuyDancingTest.mp4").unwrap();
+    frame::vid_cap_from_mp4_test("/home/kanjinaem/coding/emoterm/src/test/qtEmoGuyDancingTest.mp4").unwrap();
 }
 
 // somehow works if uri is passed directly as string ref, but not if passed from const string ref test const
--- src/service/frame_extraction.rs
+++ src/service/frame_extraction.rs
@@ -1,6 +1,16 @@
-use opencv::{highgui, prelude::*, videoio::{self, VideoCapture, VideoCaptureProperties, CAP_PROP_FPS}, Result};
+use opencv::{
+    highgui, 
+    prelude::*, 
+    videoio::{
+        self, 
+        VideoCapture, 
+        VideoCaptureProperties, 
+        CAP_PROP_FPS
+    }, 
+    Result
+};
 
-pub fn get_vid_from_mp4(file_path: &str) -> Result<()> {
+pub fn vid_cap_from_mp4(file_path: &str) -> Result<()> {
     let win_name = "capture_window";
     highgui::named_window(win_name, highgui::WINDOW_AUTOSIZE)?;
     let mut vid_player = VideoCapture::from_file_def(file_path)?;
@@ -9,18 +19,54 @@ pub fn get_vid_from_mp4(file_path: &str) -> Result<()> {
         panic!("////////////////////// can't open vid file :( //////////////////////");
     } 
 
+    let vid_fps: f64 = vid_player.get(CAP_PROP_FPS)?;
+    let frame_delay: i32 = ((1.0 / vid_fps) * 1000.0) as i32; 
+
     loop {
         let mut curr_frame = Mat::default();
         vid_player.read(&mut curr_frame)?;
         if curr_frame.size()?.width > 0 {
             highgui::imshow(win_name, &curr_frame)?;
         }
-        let vid_fps = vid_player.get(CAP_PROP_FPS)? as i32;
-        let key = highgui::wait_key(vid_fps)?;
+        
+        let key = highgui::wait_key(frame_delay)?;
         if key == 113 {
             break;
         }
     }
+
+    Ok(())
+}
+
+
+pub fn vid_cap_from_mp4_test(file_path: &str) -> Result<()> {
+    let win_name = "capture_window";
+    let mut vid_player = VideoCapture::from_file_def(file_path)?;
+    let opened = videoio::VideoCapture::is_opened(&vid_player)?;
+    if !opened {
+        panic!("////////////////////// can't open vid file :( //////////////////////");
+    } 
+
+
+    let mut curr_frame = Mat::default();
+    vid_player.read(&mut curr_frame)?; 
+    // dbg!(&curr_frame.data_typed::<opencv::CV_8UC3>()?);
+
+    // let vid_fps: f64 = vid_player.get(CAP_PROP_FPS)?;
+    // let frame_delay: i32 = ((1.0 / vid_fps) * 1000.0) as i32; 
+
+    // loop {
+    //     let mut curr_frame = Mat::default();
+    //     vid_player.read(&mut curr_frame)?;
+    //     if curr_frame.size()?.width > 0 {
+    //         highgui::imshow(win_name, &curr_frame)?;
+    //     }
+        
+    //     let key = highgui::wait_key(frame_delay)?;
+    //     if key == 113 {
+    //         break;
+    //     }
+    // }
 
     Ok(())
 }
