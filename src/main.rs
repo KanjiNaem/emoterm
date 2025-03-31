838c6df10654fdbd04ee690a64496c9bf3fd6321
John Doe <janik.jehmlich@gmail.com>
2025-03-31 02:17:26 +0200

    add basic functionality :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::path::Path;
-
 #[path = "service/frame_extraction.rs"] mod frame;
+#[path = "test/test_utils.rs"] mod test;
 // #[path = "utils/errors.rs"] mod err;
-mod utils;
+
 
 fn main() {
-    // frame::open_video(Path::new("/test/qtEmoBoyDancing.mp4"));
-    frame::play_video_test();
+    frame::get_vid_from_mp4("/home/kanjinaem/coding/emoterm/src/test/qtEmoGuyDancingTest.mp4").unwrap();
 }
+
+// somehow works if uri is passed directly as string ref, but not if passed from const string ref test const
+// whyyy???/
--- src/service/frame_extraction.rs
+++ src/service/frame_extraction.rs
@@ -1,70 +1,26 @@
-// use crate::utils::errors::*;
-use crate::utils::{errors::*, helper_fns::*};
+use opencv::{highgui, prelude::*, videoio::{self, VideoCapture, VideoCaptureProperties, CAP_PROP_FPS}, Result};
 
-use image::{DynamicImage};
-use opencv::{prelude::*, videoio::VideoCapture};
-use std::{path::Path};
-use opencv::{highgui, videoio, Result};
+pub fn get_vid_from_mp4(file_path: &str) -> Result<()> {
+    let win_name = "captureWindow";
+    highgui::named_window(win_name, highgui::WINDOW_AUTOSIZE)?;
+    let mut vid_player = VideoCapture::from_file_def(file_path)?;
+    let opened = videoio::VideoCapture::is_opened(&vid_player)?;
+    if !opened {
+        panic!("////////////////////// can't open vid file :( //////////////////////");
+    } 
 
-
-pub enum FrameIterator {
-    Video(VideoCapture),
-    AnimatedImage {
-        frames: Vec<DynamicImage>,
-        current_frame: usize,
-    },
-}
-
-impl Iterator for FrameIterator {
-    type Item = DynamicImage;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self {
-
-            FrameIterator::Video(ref mut video) => capture_video_frame(video),
-            FrameIterator::AnimatedImage {
-                ref frames,
-                ref mut current_frame,
-            } => {
-                if *current_frame == frames.len() {
-                    None
-                } else {
-                    let frame = frames.get(*current_frame).cloned();
-                    *current_frame += 1;
-                    frame
-                }
-            }
+    loop {
+        let mut curr_frame = Mat::default();
+        vid_player.read(&mut curr_frame)?;
+        if curr_frame.size()?.width > 0 {
+            highgui::imshow(win_name, &curr_frame)?;
+        }
+        let vid_fps = vid_player.get(CAP_PROP_FPS)? as i32;
+        let key = highgui::wait_key(vid_fps)?;
+        if key == 113 {
+            break;
         }
     }
-}
 
-pub fn play_video_test() -> Result<()> {
-	let window = "video capture";
-	highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
-	let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 is the default camera
-	let opened = videoio::VideoCapture::is_opened(&cam)?;
-	if !opened {
-		panic!("Unable to open default camera!");
-	}
-	loop {
-		let mut frame = Mat::default();
-		cam.read(&mut frame)?;
-		if frame.size()?.width > 0 {
-			highgui::imshow(window, &frame)?;
-		}
-		let key = highgui::wait_key(10)?;
-		if key > 0 && key != 255 {
-			break;
-		}
-	}
-	Ok(())
-}
-
-fn capture_video_frame(video: &mut VideoCapture) -> Option<DynamicImage> {
-    let mut frame = Mat::default();
-    if video.read(&mut frame).unwrap_or(false) && !frame.empty() {
-        mat_to_dynamic_image(&frame)
-    } else {
-        None
-    }
+    Ok(())
 }
